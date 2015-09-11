dfdaedc8a8e6d51f4506e71a47642b4f46b9da24
Jia Zhang <jiazhang@opera.com>
2015-09-11 15:21:36 +0800

    fixup

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (if (fboundp 'with-eval-after-load)
@@ -71,18 +78,12 @@
             (aggressive-indent-mode)
             (yas-global-mode)
             (superword-mode)))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-to-list 'default-frame-alist '(font .  "Ubuntu Mono-12"))
+(add-to-list 'default-frame-alist '(font .  "Hack-11"))
+;;(add-to-list 'default-frame-alist '(font .  "Ubuntu Mono-12"))
 ;;(set-face-attribute 'default t :font  "Ubuntu Mono-12")
-
-(require-package 'elfeed)
-(setq elfeed-feeds
-      '("http://www.geekfan.net/feed/"
-        "http://www.blackberryfans.com/portal.php?mod=rss"
-        "http://songshuhui.net/feed"
-        "http://blog.jobbole.com/feed"
-        "http://www.techug.com/feed"))
 
 (when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
   (error "Please move init-local.el to ~/.emacs.d/lisp"))
