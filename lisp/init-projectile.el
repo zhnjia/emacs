3d355207102109c42ac0b9813b7bea9a271d20a8
Jia Zhang <jiazhang@opera.com>
2015-09-25 15:04:13 +0800

    fixup

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,9 +1,21 @@
-
 (require-package 'projectile)
-(require-package 'perspective)
-(require-package 'persp-projectile)
+;(require-package 'perspective)
+;(require-package 'persp-projectile)
 
-(persp-mode)
-(require 'persp-projectile)
+;(persp-mode)
+;(require 'persp-projectile)
+
+;;(setq projectile-indexing-method 'native)
+;;(setq projectile-enable-caching t)
+
+(defvar my-project-name nil)
+(put 'my-project-name 'safe-local-variable #'stringp)
+
+(defun projectile-project-name--prefer-mine (orig-fun &rest args)
+  (or my-project-name (apply orig-fun args)))
+
+(advice-add 'projectile-project-name :around #'projectile-project-name--prefer-mine)
+
+(projectile-global-mode)
 
 (provide 'init-projectile)
