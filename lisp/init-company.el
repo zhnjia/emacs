41188de05f998806bb4a4c2e3339e04ab1c3e88c
Zhang Jia <zhnjia@gmail.com>
2015-12-03 02:12:10 +0800

    fixup

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,5 +1,5 @@
 (require-package 'company)
-(require-package 'company-jedi)
+;;(require-package 'company-jedi)
 
 (setq company-minimum-prefix-length 2)
 (setq company-idle-delay 0.0)
@@ -8,9 +8,9 @@
       (lambda()
             (global-company-mode)))
 
-(defun my/python-mode-hook ()
-   (add-to-list 'company-backends 'company-jedi))
+;;(defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
 
-(add-hook 'python-mode-hook 'my/python-mode-hook)
+;;(add-hook 'python-mode-hook 'my/python-mode-hook)
 
 (provide 'init-company)
