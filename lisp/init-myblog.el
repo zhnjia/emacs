65b7a2efd08808adece21ee769d6f9da6e625c18
Jia Zhang <jiazhang@opera.com>
2016-11-15 16:44:41 +0800

    fixup blog

--- lisp/init-myblog.el
+++ lisp/init-myblog.el
@@ -1,32 +1,29 @@
 (require 'ox-publish)
 (setq org-publish-project-alist
       '(
-        ("blog-notes"
-         :base-directory "~/org/blogs/org/"
+        ("org-ianbarton"
+         ;; Path to your org files.
+         :base-directory "~/org/myblog/org/"
          :base-extension "org"
-         :publishing-directory "~/org/blogs/"
+
+         ;; Path to your Jekyll project.
+         :publishing-directory "~/org/myblog/jekyll/"
          :recursive t
-         :publishing-function org-html-publish-to-html
+         :publishing-function org-publish-org-to-html
          :headline-levels 4
-         :auto-preamble t
-         :section-numbers nil
-         :email "zhnjia@gmail.com"
-         :auto-sitemap t
-         :sitemap-filename "sitemap.org"
-         :sitemap-title "Sitemap"
-         :sitemap-sort-files anti-chronologically
-         :sitemap-file-entry-format "%d %t"
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/hyde.css\"/>"
+         :html-extension "html"
+         :body-only t ;; Only export section between <body> </body>
          )
-        ("blog-static"
-         :base-directory "~/org/blogs/org/"
-         :base-extention "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/org/blogs/"
+
+
+        ("org-static-ian"
+         :base-directory "~/org/myblog/org/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+         :publishing-directory "~/org/myblog/"
          :recursive t
-         :publishing-function org-publish-attachment
-         )
-        ("blog"
-         :components ("blog-notes" "blog-static")
-         )))
+         :publishing-function org-publish-attachment)
+
+        ("ian" :components ("org-ianbarton" "org-static-ian"))
+        ))
 
 (provide 'init-myblog)
