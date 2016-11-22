316844fbb4943319b3f6f88590e30ab356faba53
Jia Zhang <jiazhang@opera.com>
2016-11-22 09:53:11 +0800

    counsel

---
+++ lisp/init-counsel.el
@@ -0,0 +1,18 @@
+;; projectile
+(require-package 'counsel-projectile)
+(counsel-projectile-on)
+
+;; M-x
+(global-set-key [remap execute-extended-command] 'counsel-M-x)
+
+;; swiper
+(require-package 'swiper)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c M-r") 'ivy-resume)
+
+;; imenu
+(global-set-key (kbd "C-c M-i") 'counsel-imenu)
+
+(provide 'init-counsel)
