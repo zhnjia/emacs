1c90f7d076465aa810776bfe5d1f73b0c432a86a
Jia Zhang <jiazhang@opera.com>
2016-12-20 20:21:11 +0800

    fixup

--- lisp/init-mu4e.el
+++ lisp/init-mu4e.el
@@ -2,21 +2,14 @@
 (add-to-list 'load-path "/usr/local/share/emacs/site-list/mu4e")
 (require 'mu4e)
 
-(setq
- mu4e-maildir "~/.mailbox_opera"
- mu4e-sent-folder "/sent"
- mu4e-drafts-folder "/drafts"
- mu4e-trash-folder "/trash"
- mu4e-refile-folder "/archive"
- mu4e-attachment-dir  "~/Downloads"
- mu4e-html2text-command "html2text -utf8 -width 72")
-
+;; common settings
 (setq mu4e-maildir-shortcuts
       '( ("/drafts"      . ?d)
-         ("/sent"        . ?s)
+         ("/opera/sent"        . ?s)
          ("/trash"       . ?t)
          ("/bts"         . ?b)
          ("/reviews"     . ?r)
+         ("/inbox"       . ?i)
          ("/all"         . ?a)))
 
 (setq mu4e-headers-fields
@@ -25,26 +18,67 @@
         (:from          .  22)
         (:subject       .  nil))) ;; alternatively, use :thread-subject
 
+(setq mu4e-attachment-dir     "~/Downloads"
+      ;;mu4e-sent-folder        "/sent"
+      ;;mu4e-drafts-folder      "/drafts"
+      ;;mu4e-trash-folder       "/trash"
+      ;;mu4e-refile-folder      "/archive"
+      mu4e-html2text-command  "html2text -utf8 -width 72")
+
+(setq message-kill-buffer-on-exit t)
+(setq mu4e-view-show-images t
+      mu4e-show-images t
+      mu4e-view-image-max-width 800)
+(setq mail-user-agent 'mu4e-user-agent)
+(setq mu4e-headers-date-format "%Y-%m-%d %H:%M")
+;;(setq mu4e-context-policy 'pick-first)
+
+;; accounts
+(setq mu4e-contexts
+      `( , (make-mu4e-context
+            :name "1: Opera"
+            :match-func (lambda (msg)
+                          (when msg
+                            (mu4e-message-contact-field-matches msg
+                                                                :to "jiazhang@opera.com")))
+            :vars '((user-mail-address          . "jiazhang@opera.com")
+                    (mu4e-sent-folder           . "/opera/sent")
+                    (mu4e-drafts-folder         . "/opera/drafts")
+                    (mu4e-trash-folder          . "/opera/trash")
+                    (mu4e-refile-folder         . "/opera/archive")))
+           , (make-mu4e-context
+              :name "2: Oupeng"
+              :match-func (lambda (msg)
+                            (when msg
+                              (mu4e-message-contact-field-matches msg
+                                                                  :to "jiaz@oupeng.com")))
+              :vars '((user-mail-address        . "jiaz@oupeng.com")
+                      (user-full-name           . "张佳")
+                      (mu4e-sent-folder         . "/oupeng/sent")
+                      (mu4e-drafts-folder       . "/oupeng/drafts")
+                      (mu4e-trash-folder        . "/oupeng/trash")
+                      (mu4e-refile-folder       . "/oupeng/archive")
+                      (mu4e-compose-signature   . (concat
+                                                   "张佳\n"
+                                                   "欧朋浏览器-工程师\n"
+                                                   "现在就帮你省掉70%的手机上网流量！\n"))))))
+
+;; fetching mail
 (setq mu4e-get-mail-command "offlineimap"
-      mu4e-update-interval 300
-      user-mail-address "jiazhang@opera.com"
-      user-full-name "Zhang Jia")
+      mu4e-update-interval 300)
 
 ;; smtpmail
 (require 'smtpmail)
-
 (setq message-send-mail-function 'smtpmail-send-it
       starttls-use-gnutls t
-      smtpmail-starttls-credentials '(("smtp.g.opera.com" 465 nil nil))
-      smtpmail-auth-credentials '(("smtp.g.opera.com" 465 "jiazhang@opera.com" nil))
-      smtpmail-default-smtp-server "smtp.g.opera.com"
-      smtpmail-smtp-server "smtp.g.opera.com"
-      smtpmail-smtp-service 465
+      ;;      smtpmail-starttls-credentials '(("smtp.g.opera.com" 465 nil nil))
+      ;;      smtpmail-auth-credentials '(("smtp.g.opera.com" 465 "jiazhang@opera.com" nil))
+      ;;      smtpmail-default-smtp-server "smtp.g.opera.com"
+      ;;      smtpmail-smtp-server "smtp.g.opera.com"
+      ;;      smtpmail-smtp-service 465
       )
 
-(setq smtpmail-smtp-server "mail.oupeng.com"
-      smtpmail-smtp-service 25)
-
-(setq message-kill-buffer-on-exit t)
+;;(setq smtpmail-smtp-server "mail.oupeng.com"
+;;      smtpmail-smtp-service 25)
 
 (provide 'init-mu4e)
