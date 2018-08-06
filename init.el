7ee44034ad22b835a3073c048782202395f9e993
Jia Zhang <jiazhang@opera.com>
2018-08-06 11:51:31 +0800

    week number in calendar

--- init.el
+++ init.el
@@ -96,3 +96,18 @@
 
 ;;(desktop-save-mode 1)
 (require 'gn-mode)
+
+;;show week number on Calendar
+(setq calendar-week-start-day 1)
+
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'font-lock-warning-face))
+
+(setq calendar-intermonth-header
+      (propertize "Wk"                  ; or e.g. "KW" in Germany
+                  'font-lock-face 'font-lock-keyword-face))
